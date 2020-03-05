ce2e75bd13d24417fcf24c8065dcc26ce4f1421b
kagarikoishi <61619719+kagarikoishi@users.noreply.github.com>
2020-03-05 13:51:07 +0100

    Create nombrespairs.c

---
+++ ultrabasique/nombrespairs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    int i = 0;
+    int b = 0;
+    printf("Jusqu'à quel nombre voulez-vous énumérer les nombres pairs ?")
+    scanf("%d",&i);
+    int z = i / b;
+    printf("%d/5 = %d ", i, z);
+    printf("\n");
+    return 0;
+}
